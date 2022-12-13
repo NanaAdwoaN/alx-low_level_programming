7d79bbd40225b0586d06f77f7dc22aa73cd52191
NanaAdwoaN <nanaaansiah7@gmail.com>
2022-12-12 18:59:01 -0800

    brand

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main -entry
- *
- * Return 0
+ * Return 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
