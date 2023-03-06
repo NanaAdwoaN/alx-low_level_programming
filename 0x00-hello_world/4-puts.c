a095b701176df3087a04af50e6e8e9e76d883111
NanaAdwoaN <nanaaansiah7@gmail.com>
2023-03-06 09:11:28 -0800

    task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/**
- * main -entry
- * Return 0 if exited properly, non-zero otherwise
- */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main entry
+ * main - Entry point
  *
- * return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an init: %d byte(s)\n", sizeof(int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
